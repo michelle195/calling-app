35786e89394596b039149c6735e7ab3cac49c52f
mlee692 <mlee692@gatech.edu>
2021-10-04 01:29:16 -0400

    made it pretty

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './phone.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a phone number:
         </p>
         <a
           className="App-link"
